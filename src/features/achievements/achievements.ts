73c1f91cf4b171a292ca0eb9ac5155b4da51e889
ncode <nirajkrz@users.noreply.github.com>
2026-05-31 21:59:32 -0500

    Add achievements data structure

---
+++ src/features/achievements/achievements.ts
@@ -0,0 +1,20 @@
+export const achievements = [
+
+ {
+   id:"first_lesson",
+   title:"First Lesson",
+   xp:0
+ },
+
+ {
+   id:"100xp",
+   title:"100 XP Earned",
+   xp:100
+ },
+
+ {
+   id:"500xp",
+   title:"500 XP Earned",
+   xp:500
+ }
+];
