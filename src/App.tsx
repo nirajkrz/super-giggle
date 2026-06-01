e43677679f99d1b91b1e5bc655a726d14705989e
ncode <nirajkrz@users.noreply.github.com>
2026-05-31 21:44:29 -0500

    Create App component with routing

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import AppRoutes
+from "./routes/AppRoutes";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
